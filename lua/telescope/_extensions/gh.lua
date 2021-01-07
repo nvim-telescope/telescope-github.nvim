19ad1a9bbfd48e7475e5c328f9793f6b551658c3
windwp <baronnight19@gmail.com>
2021-01-07 18:15:34 +0700

    change to use extensions export #8

--- lua/telescope/_extensions/gh.lua
+++ lua/telescope/_extensions/gh.lua
@@ -1,12 +1,11 @@
-local builtin = require('telescope.builtin')
 local gh_b= require('telescope._extensions.gh_builtin')
 
 
 
 return require('telescope').register_extension {
-    setup = function()
-      builtin.gh_gist =gh_b.gh_gist
-      builtin.gh_issues = gh_b.gh_issues
-      builtin.gh_pull_request =gh_b.gh_pull_request
-    end;
+    exports= {
+      gist = gh_b.gh_gist ,
+      issues = gh_b.gh_issues,
+      pull_request = gh_b.gh_pull_request ,
+    },
 }
