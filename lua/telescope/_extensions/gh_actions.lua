db1470244ebe15b9c0f5a1116adfad53071ae015
windwp <windwp@users.noreply.github.com>
2021-01-07 17:43:48 +0700

    display pull request result #6 (#7)
    display result on quickfix list

--- lua/telescope/_extensions/gh_actions.lua
+++ lua/telescope/_extensions/gh_actions.lua
@@ -12,13 +12,46 @@ A.gh_pr_checkout = function(prompt_bufnr)
   if vim.tbl_isempty(tmp_table) then
     return
   end
+
+  local qf_entry = {{
+      text="Checking out pull request #" .. tmp_table[1] ..", please wait ..."
+  }}
+
+  local on_output = function(_, line)
+    table.insert(qf_entry,{
+        text = line
+      })
+    pcall(vim.schedule_wrap( function()
+      vim.fn.setqflist(qf_entry,"r")
+     end))
+  end
+
   local job = Job:new({
       enable_recording = true ,
       command = "gh",
-      args = {"pr", "checkout" ,tmp_table[1]}
+      args = {"pr", "checkout" ,tmp_table[1]},
+      on_stdout = on_output,
+      on_stderr = on_output,
+
+      on_exit = function(_,status)
+        if status == 0 then
+          pcall(vim.schedule_wrap( function()
+                vim.cmd[[cclose]]
+           end))
+           print("Pull request completed")
+        end
+      end,
     })
-  -- need to display result in quickfix
-  job:sync()
+
+  vim.fn.setqflist(qf_entry,"r")
+  vim.cmd[[copen]]
+  local timer = vim.loop.new_timer()
+  timer:start(200, 0, vim.schedule_wrap(function()
+    -- increase timeout to 10000ms and wait interval to 20
+    -- default value is 5000ms and 10
+    job:sync(10000,20)
+  end))
+
 end
 
 
